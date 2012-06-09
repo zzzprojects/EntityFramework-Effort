9fb259516b0a44060660a1fe2d337aebdb4bc175
tamasflamich <none@none>
2012-06-09 18:38:28 +0200

    NuGet package created

--- Main/Source/Effort/Properties/AssemblyInfo.cs
+++ Main/Source/Effort/Properties/AssemblyInfo.cs
@@ -57,6 +57,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta2")] 
 
 [assembly: InternalsVisibleTo("Effort.Test")]

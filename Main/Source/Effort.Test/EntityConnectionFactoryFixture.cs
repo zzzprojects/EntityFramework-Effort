53ca5e8ff6345daa974209362ac48b9d964724e5
tamasflamich <none@none>
2012-07-31 21:51:30 +0200

    EntityConnectionFactory unit tests

---
+++ Main/Source/Effort.Test/EntityConnectionFactoryFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.EntityClient;
+using System.Data.Objects;
+
+namespace Effort.Test
+{
+    [TestClass]
+    public class EntityConnectionFactoryFixture
+    {
+        [TestMethod]
+        public void CreateTransientEntityConnection()
+        {
+            EntityConnection connection = EntityConnectionFactory.CreateTransient("name=NorthwindEntities");
+        }
+
+        [TestMethod]
+        public void EntityConnectionFactoryInitializesDataScheme()
+        {
+            EntityConnection connection = EntityConnectionFactory.CreateTransient("name=NorthwindEntities");
+
+            using (ObjectContext context = new ObjectContext(connection))
+            {
+                Assert.IsTrue(context.DatabaseExists());
+                Assert.AreEqual(0, context.CreateObjectSet<Data.Products>().Count(), "Zero rows in the fake table");
+            }
+        }
+    }
+}

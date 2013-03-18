6a24278141b708d9ab8fde411930a9448482bbd8
tamasflamich <none@none>
2013-03-18 19:47:57 +0100

    Build script now creates Zip package

--- Main/Build/Build.cmd
+++ Main/Build/Build.cmd
@@ -1,3 +1,3 @@
 set msbuild=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild
 
-%msbuild% Packages.msbuild /v:minimal /maxcpucount /nodeReuse:false %*
+%msbuild% Build.targets /v:minimal /maxcpucount /nodeReuse:false %*

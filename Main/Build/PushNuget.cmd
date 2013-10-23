540377e18a53f65bd7fba8bf4bfa3e8af5e260b0
tamasflamich <none@none>
2013-10-23 16:27:27 +0200

    1.0.0-beta5 Updated NMemory to 1.0.0-beta9

--- Main/Build/PushNuget.cmd
+++ Main/Build/PushNuget.cmd
@@ -1,13 +1,8 @@
 @echo OFF
 
-set package=
-for %%x in (.\Packages\*.nupkg) do if not defined package set "package=%%x"
-
-if not defined package (
-    echo NuGet package has not been created!
-    exit /b -1
-)
+for %%x in (.\Packages\*.nupkg) do echo %%x
 
 set /p apikey=Enter NuGet.org API key:
 ..\Source\.nuget\NuGet.exe setApiKey %apikey%
-..\Source\.nuget\NuGet.exe push ".\%package%"
+
+for %%x in (.\Packages\*.nupkg) do ..\Source\.nuget\NuGet.exe push ".\%%x"

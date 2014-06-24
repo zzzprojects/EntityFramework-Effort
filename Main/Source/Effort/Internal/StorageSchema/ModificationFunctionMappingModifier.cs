4759c4a2cef78f48a4f5fcd584eed3e9558dac19
Augustin Šulc <sulc.gusta@gmail.com>
2014-06-25 01:18:43 +0200

    Modification function mappings are removed from metadata when loading. This enables effort to mock EF models with stored procedures mapped for entity Insert/Update/Delete

---
+++ Main/Source/Effort/Internal/StorageSchema/ModificationFunctionMappingModifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Effort.Internal.Common.XmlProcessing;
+
+namespace Effort.Internal.StorageSchema
+{
+    /// <summary>
+    /// Removes function mapping for Insert, Update, and Delete.
+    /// This is required for being able to save changes to EFFORT context
+    /// based on model with defined modification function mappings.
+    /// </summary>
+    internal class ModificationFunctionMappingModifier : IElementModifier
+    {
+        public void Modify(XElement element, IModificationContext context)
+        {
+            var functionMappings = element.Descendants(XName.Get("ModificationFunctionMapping", "http://schemas.microsoft.com/ado/2009/11/mapping/cs")).ToList();
+            foreach (var mappingElement in functionMappings)
+            {
+                mappingElement.Remove();
+            }
+        }
+    }
+}

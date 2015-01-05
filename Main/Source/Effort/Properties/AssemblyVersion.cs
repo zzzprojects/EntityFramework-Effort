b070061568933ef885451d58c0979ec605e5e0b0
Tamas Flamich <tamasflamich@outlook.com>
2015-01-05 23:31:20 +0100

    1.1.3 release

--- Main/Source/Effort/Properties/AssemblyVersion.cs
+++ Main/Source/Effort/Properties/AssemblyVersion.cs
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.2")]
-[assembly: AssemblyInformationalVersion("1.1.2")] 
+[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyInformationalVersion("1.1.3")] 

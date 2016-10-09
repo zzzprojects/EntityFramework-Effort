25c83c39165f9e0be67a03e243dc394b7de7ed0c
Tamas Flamich <tamasflamich@hotmail.com>
2016-10-09 20:57:18 +0200

    1.2.0 release

--- Main/Source/Effort/Properties/AssemblyVersion.cs
+++ Main/Source/Effort/Properties/AssemblyVersion.cs
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.4")]
-[assembly: AssemblyInformationalVersion("1.1.4")] 
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")] 

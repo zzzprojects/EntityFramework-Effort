0c150fefb2591e8b0de67fcb76353a568ef6a486
zzzprojects <sales@zzzprojcts.com>
2017-12-29 13:30:13 -0500

    Update ignore
    Update ignore

--- Main/Source/Effort.Lab.EF6/Form_Request_LazyLoading.cs
+++ Main/Source/Effort.Lab.EF6/Form_Request_LazyLoading.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace Effort.Lab.EF6
 {
@@ -16,37 +11,47 @@ namespace Effort.Lab.EF6
         public Form_Request_LazyLoading()
         {
             InitializeComponent();
+
+            var connection = DbConnectionFactory.CreateTransient();
+
             // CLEAN
-            using (var context = new EntityContext())
+            using (var context = new EntityContext(connection))
             {
-                context.EntitySimple.RemoveRange(context.EntitySimple);
+                context.EntitySimples.RemoveRange(context.EntitySimples);
+                context.EntitySimpleLazy.RemoveRange(context.EntitySimpleLazy);
                 context.SaveChanges();
             }
 
             // SEED
-            using (var context = new EntityContext())
+            using (var context = new EntityContext(connection))
             {
-                context.EntitySimple.Add(new EntitySimple() { ColumnInt = 1 });
-                context.EntitySimple.Add(new EntitySimple() { ColumnInt = 2 });
-                context.EntitySimple.Add(new EntitySimple() { ColumnInt = 3 });
+                var entity1 = context.EntitySimples.Add(new EntitySimple {ColumnInt = 1, EntitySimpleLazy = new List<EntitySimpleLazy>()});
+                entity1.EntitySimpleLazy.Add(new EntitySimpleLazy {ColumnInt = 10});
+                entity1.EntitySimpleLazy.Add(new EntitySimpleLazy {ColumnInt = 20});
+                context.EntitySimples.Add(new EntitySimple {ColumnInt = 2});
+                context.EntitySimples.Add(new EntitySimple {ColumnInt = 3});
                 context.SaveChanges();
+
+                // Proxy type are not created since entities (without proxy) already exists in the context
+                var list = context.EntitySimples.ToList();
             }
 
             // TEST
-            using (var context = new EntityContext())
+            using (var context = new EntityContext(connection))
             {
-
-
+                // Proxy type are created since entities are loaded from the memory
+                var list = context.EntitySimples.ToList();
             }
         }
 
         public class EntityContext : DbContext
         {
-            public EntityContext() : base("CodeFirstEntities")
+            public EntityContext(DbConnection connection) : base(connection, true)
             {
+                //this.Configuration.LazyLoadingEnabled = true;
             }
 
-            public DbSet<EntitySimple> EntitySimple { get; set; }
+            public DbSet<EntitySimple> EntitySimples { get; set; }
 
             public DbSet<EntitySimpleLazy> EntitySimpleLazy { get; set; }
 
@@ -68,6 +73,7 @@ namespace Effort.Lab.EF6
 
             public virtual ICollection<EntitySimpleLazy> EntitySimpleLazy { get; set; }
         }
+
         public class EntitySimpleLazy
         {
             public int ID { get; set; }
--- Main/Source/Effort.Lab.EF6/__Form_Template.cs
+++ Main/Source/Effort.Lab.EF6/__Form_Template.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System.Data.Common;
 using System.Data.Entity;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Effort.Lab.EF6
@@ -16,38 +10,39 @@ namespace Effort.Lab.EF6
         public __Form_Template()
         {
             InitializeComponent();
+
+            DbConnection connection = Effort.DbConnectionFactory.CreateTransient();
+
             // CLEAN
-            using (var context = new EntityContext())
+            using (var context = new EntityContext(connection))
             {
-                context.EntitySimple.RemoveRange(context.EntitySimple);
+                context.EntitySimples.RemoveRange(context.EntitySimples);
                 context.SaveChanges();
             }
 
             // SEED
-            using (var context = new EntityContext())
+            using (var context = new EntityContext(connection))
             {
-                context.EntitySimple.Add(new EntitySimple() { ColumnInt = 1 });
-                context.EntitySimple.Add(new EntitySimple() { ColumnInt = 2 });
-                context.EntitySimple.Add(new EntitySimple() { ColumnInt = 3 });
+                context.EntitySimples.Add(new EntitySimple {ColumnInt = 1});
+                context.EntitySimples.Add(new EntitySimple {ColumnInt = 2});
+                context.EntitySimples.Add(new EntitySimple {ColumnInt = 3});
                 context.SaveChanges();
             }
 
             // TEST
-            using (var context = new EntityContext())
+            using (var context = new EntityContext(connection))
             {
-
-
             }
         }
 
         public class EntityContext : DbContext
         {
-            public EntityContext() : base("CodeFirstEntities")
+            public EntityContext(DbConnection connection) : base(connection, true)
             {
             }
 
-            public DbSet<EntitySimple> EntitySimple { get; set; }
-             
+            public DbSet<EntitySimple> EntitySimples { get; set; }
+
 
             protected override void OnModelCreating(DbModelBuilder modelBuilder)
             {
@@ -64,7 +59,6 @@ namespace Effort.Lab.EF6
         {
             public int ID { get; set; }
             public int ColumnInt { get; set; }
-             
-        } 
+        }
     }
 }

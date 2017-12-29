6496aefb3cb17b89b6fce45f5f6247456ccd7567
zzzprojects <sales@zzzprojcts.com>
2017-12-29 13:51:16 -0500

    Update Package Description & Version
    Update Package Description & Version

--- Main/Source/Effort/Properties/AssemblyVersion.cs
+++ Main/Source/Effort/Properties/AssemblyVersion.cs
@@ -36,5 +36,5 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")] 
+[assembly: AssemblyFileVersion("1.3.1")]
+[assembly: AssemblyInformationalVersion("1.3.1")] 

39760589caaf5c83a1d13f1ae5d115a34457de39
zzzprojects <sales@zzzprojcts.com>
2018-06-27 17:06:24 -0400

    Update assembly
    Update assembly

--- Main/Source/Effort/Properties/AssemblyVersion.cs
+++ Main/Source/Effort/Properties/AssemblyVersion.cs
@@ -35,5 +35,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.4")]
-[assembly: AssemblyInformationalVersion("1.3.4")]
+[assembly: AssemblyFileVersion("1.3.5")]
+[assembly: AssemblyInformationalVersion("1.3.5")]

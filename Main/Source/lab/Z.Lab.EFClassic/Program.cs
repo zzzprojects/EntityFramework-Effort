4dec2dea0040a662e524e79679bc3a270f242abe
Jonathan Magnan <info@zzzprojects.com>
2018-10-28 10:27:29 -0400

    Add lab

--- Main/Source/lab/Z.Lab.EFClassic/Program.cs
+++ Main/Source/lab/Z.Lab.EFClassic/Program.cs
@@ -16,7 +16,7 @@ namespace Z.Lab.EFClassic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_Request_Register());
         }
     }
 }

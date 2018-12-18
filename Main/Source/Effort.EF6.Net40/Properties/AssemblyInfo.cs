b7a8f3658408f6bbc5a683f9eda1042bacfe757f
Jonathan Magnan <info@zzzprojects.com>
2018-12-18 15:33:55 -0500

    update assembly version

--- Main/Source/Effort.EF6.Net40/Properties/AssemblyInfo.cs
+++ Main/Source/Effort.EF6.Net40/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("896d3ca3-a5bd-4aac-b384-8488ad59ee87")]
 
-[assembly: AssemblyVersion("2.0.3.0")]
-[assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
